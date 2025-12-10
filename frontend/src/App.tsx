54249fcb2c5a565755ceb70c964c9e9e866e82b5
Swesan Pathmanathan <201360on50@gmail.com>
2025-12-10 01:20:46 -0500

    fix: Add ClassQuestions route and link from Classes page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Layout from './components/Layout'
 import Home from './pages/Home'
 import Classes from './pages/Classes'
 import ClassDetails from './pages/ClassDetails'
+import ClassQuestions from './pages/ClassQuestions'
 import Generate from './pages/Generate'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/classes" element={<Classes />} />
         <Route path="/classes/:id" element={<ClassDetails />} />
+        <Route path="/classes/:id/questions" element={<ClassQuestions />} />
         <Route path="/generate" element={<Generate />} />
       </Routes>
     </Layout>
