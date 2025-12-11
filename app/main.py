5c05f59501ede9267286f17ac21a6d00aba7604e
Swesan Pathmanathan <201360on50@gmail.com>
2025-12-10 20:03:28 -0500

    fix(issue-45): fix deprecation warning for HTTP_422 status code

--- app/main.py
+++ app/main.py
@@ -127,7 +127,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
     )
 
     return JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
         content={
             "error": {
                 "code": "ValidationError",
